e033027c6aa508da39c982417fa54f9602f95765
Emna labidi <beauteloisir2@gmail.com>
2022-01-13 14:50:45 +0100

    postgresq maj

--- connection.php
+++ connection.php
@@ -3,5 +3,5 @@ error_reporting(1);
 //$con=mysql_connect('localhost', 'root', '','election') or die(mysqli_error());
 $con=pg_connect('localhost', 'root', '','election',) 
 // this is database connection stored in a variable con.
-
+///
 ?>
